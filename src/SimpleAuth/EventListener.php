900bc41ebe47e7f54747e3146a2fd467a878bd06
xxFlare <appletreegold@gmail.com>
2015-03-16 11:46:01 -0400

    Update EventListener.php

--- src/SimpleAuth/EventListener.php
+++ src/SimpleAuth/EventListener.php
@@ -1,18 +1,36 @@
 <?php
 
 /*
- * SimpleAuth plugin for PocketMine-MP
- * Copyright (C) 2014 PocketMine Team <https://github.com/PocketMine/SimpleAuth>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ case "register":
+				if($sender instanceof Player){
+					if($this->isPlayerRegistered($sender)){
+						$sender->sendMessage(TextFormat::RED . $this->getMessage("register.error.registered"));
+
+						return true;
+					}
+
+					$password = implode(" ", $args);
+					if(strlen($password) < $this->getConfig()->get("minPasswordLength")){
+						$sender->sendMessage($this->getMessage("register.error.password"));
+						return true;
+					}
+
+					if($this->registerPlayer($sender, $password) and $this->authenticatePlayer($sender)){
+						return true;
+					}else{
+						$sender->sendMessage(TextFormat::RED . $this->getMessage("register.error.general"));
+						return true;
+					}
+				}else{
+					$sender->sendMessage(TextFormat::RED . "This command only works in-game.");
+
+					return true;
+				}
+				break;
+		}
+
+		return false;
+	}
 */
 
 namespace SimpleAuth;
